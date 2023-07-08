4e5b84a87454b6cf58b031c6582c3df51222593f
Morgan <mhanssler@gmail.com>
2023-07-08 13:32:57 -0700

    Create fizzbuzz.py
    test to understand how to use github and pycharm

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+''' This is a quick program to help practice loading and making changes with PyCharm '''
+
+a = 10
+for i in range(a):
+  if i%2 == 0:
+  print("Fizz")
+elif i %2 != 0:
+  print("Buzz")
+  
